b616dd1c17c30cfa8ec5110d2dbba3ecd23854fd
Julia Karine Peres <103675133+Juliakp1@users.noreply.github.com>
2025-02-26 12:20:46 -0300

    on the verge of a breakthrough

--- EscapeRoom/Assets/Scripts/SlidingPuzzleManager.cs
+++ EscapeRoom/Assets/Scripts/SlidingPuzzleManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
   [SerializeField] private Transform gameTransform;
@@ -11,6 +12,7 @@ public class GameManager : MonoBehaviour {
   private int emptyLocation;
   private int size;
   private bool shuffling = false;
+  private bool currentlyInteracting = false; 
   public UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInteractor rightRayInteractor; // Reference to the XRRayInteractor on the right hand controller
   public UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInteractor leftRayInteractor;  // Reference to the XRRayInteractor on the left hand controller
 
@@ -19,138 +21,220 @@ public class GameManager : MonoBehaviour {
     // This is the width of each tile.
     float width = 1 / (float)size;
     for (int row = 0; row < size; row++) {
-      for (int col = 0; col < size; col++) {
-        Transform piece = Instantiate(piecePrefab, gameTransform);
-        pieces.Add(piece);
-        // Pieces will be in a game board going from -1 to +1.
-        piece.localPosition = new Vector3(0,
-                                          -1 + (2 * width * col) + width,
-                                          +1 - (2 * width * row) - width);
-        piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
-        piece.name = $"{(row * size) + col}";
-        // We want an empty space in the bottom right.
-        if ((row == size - 1) && (col == size - 1)) {
-          emptyLocation = (size * size) - 1;
-          piece.gameObject.SetActive(false);
-        } else {
-          // We want to map the UV coordinates appropriately, they are 0->1.
-          float gap = gapThickness / 2;
-          Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
-          Vector2[] uv = new Vector2[4];
-          // UV coord order: (0, 1), (1, 1), (0, 0), (1, 0)
-          uv[0] = new Vector2((width * col) + gap, 1 - ((width * (row + 1)) - gap));
-          uv[1] = new Vector2((width * (col + 1)) - gap, 1 - ((width * (row + 1)) - gap));
-          uv[2] = new Vector2((width * col) + gap, 1 - ((width * row) + gap));
-          uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
-          // Assign our new UVs to the mesh.
-          mesh.uv = uv;
+        for (int col = 0; col < size; col++) {
+            Transform piece = Instantiate(piecePrefab, gameTransform);
+            pieces.Add(piece);
+            // Pieces will be in a game board going from -1 to +1.
+            piece.localPosition = new Vector3(0,
+                                              -1 + (2 * width * col) + width,
+                                              +1 - (2 * width * row) - width);
+            piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+            piece.name = $"{(row * size) + col}";
+
+            TextMeshPro textComponent = piece.GetComponentInChildren<TextMeshPro>();
+            textComponent.text = piece.name;
+
+            // We want an empty space in the bottom right.
+            if ((row == size - 1) && (col == size - 1)) {
+                emptyLocation = (size * size) - 1;
+                piece.gameObject.SetActive(false);
+            } else {
+                // UV Calculation based on the grid position
+                Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                Vector2[] uv = new Vector2[4];
+                // UV coord order: (0, 1), (1, 1), (0, 0), (1, 0)
+                uv[0] = new Vector2((float)col / size, 1 - (float)(row + 1) / size);  // Top-left corner
+                uv[1] = new Vector2((float)(col + 1) / size, 1 - (float)(row + 1) / size);  // Top-right corner
+                uv[2] = new Vector2((float)col / size, 1 - (float)row / size);  // Bottom-left corner
+                uv[3] = new Vector2((float)(col + 1) / size, 1 - (float)row / size);  // Bottom-right corner
+
+                // Assign our new UVs to the mesh.
+                mesh.uv = uv;
+            }
         }
-      }
     }
   }
+
+  void SwapPieceAtIndex0WithName(string targetName)
+{
+    // Find the index of the piece with the name "targetName"
+    int targetIndex = -1;
+    for (int i = 0; i < pieces.Count; i++)
+    {
+        if (pieces[i].name == targetName)
+        {
+            targetIndex = i;
+            break;
+        }
+    }
+
+    // If we found the target piece
+    if (targetIndex != -1)
+    {
+        // 1. Swap their positions in the scene
+        Vector3 tempPosition = pieces[0].transform.position;
+        pieces[0].transform.position = pieces[targetIndex].transform.position;
+        pieces[targetIndex].transform.position = tempPosition;
+
+        // 2. Swap their spots in the list (GameObject references)
+        Transform temp = pieces[0];
+        pieces[0] = pieces[targetIndex];
+        pieces[targetIndex] = temp;
+
+        Debug.Log("Swapped pieces at index 0 and " + targetName);
+    }
+    else
+    {
+        Debug.LogError("Piece with name " + targetName + " not found!");
+    }
+  }
+
+  void SortPiecesByName(){
+    
+    bool sorted = false;
+    while (!sorted)
+    {
+        sorted = true;  // Assume the list is sorted until proven otherwise
+        
+        // Loop through the pieces list and compare each one
+        for (int i = 1; i < pieces.Count; i++)
+        {
+            if (string.Compare(pieces[0].name, pieces[i].name) > 0)
+            {
+                SwapPieceAtIndex0WithName(pieces[i].name);
+                print($"swapped {i}");
+                sorted = false;  // List is not sorted yet, keep looping
+                break; 
+            }
+            print($"already correct: {i}");
+        }
+    }
+  }
+
+
 
   // Start is called before the first frame update
   void Start() {
     pieces = new List<Transform>();
-    size = 3;
+    size = 5;
     CreateGamePieces(0.01f);
+
+    foreach (var piece in pieces)
+    {
+        // Make sure each piece has an XRSimpleInteractable
+        var interactable = piece.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>();
+        if (interactable != null)
+        {
+            interactable.selectEntered.AddListener(OnPieceSelected);
+            interactable.selectExited.AddListener(OnPieceDeselected); 
+        }
+    }
+
+    SortPiecesByName();
+    
   }
 
   // Update is called once per frame
   void Update() {
 
-    bool done = CheckCompletion();
-    if (done){
-      // Debug.Log("yay!!");
-    }
-
     // Check for completion.
-    else if (!shuffling && done) {
-      shuffling = true;
-      StartCoroutine(WaitShuffle(0.5f));
+    if (!shuffling && CheckCompletion()) {
+      print("You gosh darn did it! - Respectuflly, the sliding puzzle");
     }
 
-    // Check if the right hand ray interactor is interacting with a valid object
-    if (rightRayInteractor.interactablesSelected.Count > 0)
-        {
-            UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable hoveredInteractable = rightRayInteractor.interactablesSelected[0] as UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable;
-            GameObject hoveredObject = null;  // Declare hoveredObject outside the if block
-            if (hoveredInteractable != null)
-            {
-                hoveredObject = hoveredInteractable.gameObject;
-            }
+  }
 
-            if (hoveredObject != null)
-            {
-                // Check if we hit one of our pieces
-                for (int i = 0; i < pieces.Count; i++)
-                {
-                    if (pieces[i].gameObject == hoveredObject)
-                    {
-                        // Check each direction to see if valid move.
-                        if (SwapIfValid(i, -size, size)) { break; }
-                        if (SwapIfValid(i, +size, size)) { break; }
-                        if (SwapIfValid(i, -1, 0)) { break; }
-                        if (SwapIfValid(i, +1, size - 1)) { break; }
-                    }
-                }
-            }
-        }
+  private void OnPieceSelected(SelectEnterEventArgs args){
+      for (int i = 0; i < pieces.Count; i++)
+      {
+          var piece = pieces[i];
+          if (piece.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRSimpleInteractable>().isSelected && !currentlyInteracting)
+          {
+              // Check each direction to see if valid move
+              if (SwapIfValid(i, -size, size)) { break; }
+              if (SwapIfValid(i, +size, size)) { break; }
+              if (SwapIfValid(i, -1, 0)) { break; }
+              if (SwapIfValid(i, +1, size - 1)) { break; }
+              currentlyInteracting = true;
+          }
+      }
+  }
 
+
+  private void OnPieceDeselected(SelectExitEventArgs args) {
+      currentlyInteracting = false;
   }
 
   // colCheck is used to stop horizontal moves wrapping.
   private bool SwapIfValid(int i, int offset, int colCheck) {
+    // Ensure the swap does not wrap around horizontally
     if (((i % size) != colCheck) && ((i + offset) == emptyLocation)) {
-      // Swap them in game state.
-      (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
-      // Swap their transforms.
-      (pieces[i].localPosition, pieces[i + offset].localPosition) = ((pieces[i + offset].localPosition, pieces[i].localPosition));
-      // Update empty location.
-      emptyLocation = i;
-      return true;
+        // Swap in game state
+        (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+
+        // Swap transforms
+        (pieces[i].localPosition, pieces[i + offset].localPosition) = (pieces[i + offset].localPosition, pieces[i].localPosition);
+
+        // Update empty location
+        emptyLocation = i;
+        return true;
     }
+
     return false;
-  }
+}
+
 
   // We name the pieces in order so we can use this to check completion.
   private bool CheckCompletion() {
-    for (int i = 0; i < pieces.Count; i++) {
-      if (pieces[i].name != $"{i}") {
+    if (pieces.Count - 1 != emptyLocation) {
         return false;
-      }
     }
+
+    for (int i = 0; i < pieces.Count; i++) {
+        int expectedRow = i / size;
+        int expectedCol = i % size;
+        Vector3 expectedPosition = new Vector3(
+            -1 + (2 * (1f / size) * expectedCol) + (1f / size),
+            +1 - (2 * (1f / size) * expectedRow) - (1f / size),
+            0
+        );
+
+        if (pieces[i].localPosition != expectedPosition) {
+            return false;
+        }
+    }
+
     return true;
   }
 
-  private IEnumerator WaitShuffle(float duration) {
-    yield return new WaitForSeconds(duration);
-    Shuffle();
-    shuffling = false;
-  }
+  // private IEnumerator WaitShuffle(float duration) {
+  //   yield return new WaitForSeconds(duration);
+  //   Shuffle(100);
+  //   shuffling = false;
+  // }
 
-  // Brute force shuffling.
-  private void Shuffle() {
-    int count = 0;
-    int last = 0;
-    while (count < (size * size * size)) {
-      // Pick a random location.
-      int rnd = Random.Range(0, size * size);
-      // Only thing we forbid is undoing the last move.
-      if (rnd == last) { continue; }
-      last = emptyLocation;
-      // Try surrounding spaces looking for valid move.
-      if (SwapIfValid(rnd, -size, size)) {
-        count++;
-      } else if (SwapIfValid(rnd, +size, size)) {
-        count++;
-      } else if (SwapIfValid(rnd, -1, 0)) {
-        count++;
-      } else if (SwapIfValid(rnd, +1, size - 1)) {
-        count++;
-      }
-    }
-  }
+  // // Brute force shuffling.
+  // private void Shuffle(int shuffleAmount) {
+  //   int count = 0;
+  //   int last = 0;
+  //   while (count < shuffleAmount) {
+  //     // Pick a random location.
+  //     int rnd = Random.Range(0, size * size);
+  //     // Only thing we forbid is undoing the last move.
+  //     if (rnd == last) { continue; }
+  //     last = emptyLocation;
+  //     // Try surrounding spaces looking for valid move.
+  //     if (SwapIfValid(rnd, -size, size)) {
+  //       count++;
+  //     } else if (SwapIfValid(rnd, +size, size)) {
+  //       count++;
+  //     } else if (SwapIfValid(rnd, -1, 0)) {
+  //       count++;
+  //     } else if (SwapIfValid(rnd, +1, size - 1)) {
+  //       count++;
+  //     }
+  //   }
+  // }
 }
 
 
